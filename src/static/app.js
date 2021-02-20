02244ef67b480a3667ab28a385dcd6c46ac03da0
Adaendra <maxime.guilbert@neuf.fr>
2021-02-20 17:17:03 -0500

    WIP - Vue.js

--- src/static/app.js
+++ src/static/app.js
@@ -1,5 +1,15 @@
 new Vue({
     el: '#components-demo',
-    template: '<button-counter></button-counter>'
+    template: `
+    <div id="routes">
+        <h1>Home Page</h1>
+          <p>Welcome home!</p>
+          <ul>
+             <li><router-link to="/contact">Contact</router-link></li>
+             <li><router-link to="/faq">FAQ</router-link></li>
+          </ul>
+        <button-counter></button-counter>
+    </div>
+    `
     })
 
