779dc2e8f30c11a999e11e9f28ce22c6574e2a3e
Adaendra <maxime.guilbert@neuf.fr>
2021-02-20 10:45:03 -0500

    Mise en place de Vue.js et première version d'une page vue.js pouvant être utilisée par flask

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 if __name__ == '__main__':
     socketio.run(app)
