4b5969474fe3d76fa9654cd12db969040fdb4ab4
Adaendra <maxime.guilbert@neuf.fr>
2021-02-27 08:25:32 -0500

    Update dossier des routes

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask, url_for, render_template, request, Response
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'f06080df-5d2c-4c14-a97f-b6a832946d2f'
 socketio = SocketIO(app)
 
-# Import all controllers 
-from apps.rest_controllers import *
-from apps.socket_controllers import *
+# Import all routes
+from apps.rest_routes import *
+from apps.socket_routes import *
