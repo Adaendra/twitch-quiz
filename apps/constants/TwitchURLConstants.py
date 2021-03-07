0e1175d125e85d83271aafd21761a2e7c69cba81
Adaendra <maxime.guilbert@neuf.fr>
2021-03-07 13:20:52 -0500

    WIP - Corrections de TODO

--- apps/constants/TwitchURLConstants.py
+++ apps/constants/TwitchURLConstants.py
@@ -1,3 +1,4 @@
 AUTH_TOKEN_GENERATION_URL = "https://id.twitch.tv/oauth2/token"
 CUSTOM_REWARD_URL = "https://api.twitch.tv/helix/channel_points/custom_rewards"
 CUSTOM_REWARD_REGISTRATION_URL = "https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions"
+STREAM_DATA_URL = "https://api.twitch.tv/helix/streams"
