dfec81dee6b8606be71fc0c6490bc5d05ee44653
Adaendra <maxime.guilbert@neuf.fr>
2021-03-07 12:36:27 -0500

    WIP - Unit tests

---
+++ tests/services/test_twitch_auth_service.py
@@ -0,0 +1,13 @@
+from apps.services.TwitchAuthService import generateRedemptionToken
+
+
+class TestTwitchAuthService:
+
+    def test_generateRedemptionToken_ok(self, mocker):
+        mockResponse = mocker.patch(
+            'apps.services.TwitchAuthService.doHttpPost'
+        )
+        mockResponse.return_value.status_code = 200
+        mockResponse.return_value.json.return_value = {'access_token' : "test_token"}
+
+        assert generateRedemptionToken() == "test_token"
