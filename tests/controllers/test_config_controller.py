6dead09bceae84f932b3c37557a5989fdc4d1015
Adaendra <maxime.guilbert@neuf.fr>
2021-03-07 15:59:39 -0500

    WIP - Unit tests

--- tests/controllers/test_config_controller.py
+++ tests/controllers/test_config_controller.py
@@ -1,3 +1,7 @@
+from subprocess import call
+from unittest.mock import call
+from apps.constants.RewardsConstants import PLAYER_REGISTRATION_REWARD_PARAM_NAME
+
 from apps.controllers.ConfigController import configController
 
 
@@ -5,65 +9,98 @@ class TestConfigController:
 
     # ----- isAllRequiredConfigDefined ----- #
     def test_isAllRequiredConfigDefined_ok(self, mocker):
-        mocker.patch(
+        mock_client_id = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientId',
             return_value="client_id"
         )
-        mocker.patch(
+        mock_client_secret = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientSecret',
             return_value="client_secret"
         )
-        mocker.patch(
+        mock_reward_cost = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getRewardCost',
             return_value=5000
         )
 
         assert configController.isAllRequiredConfigDefined() is True
 
+        assert mock_client_id.call_count == 1
+        assert mock_client_id.call_args == call()
+
+        assert mock_client_secret.call_count == 1
+        assert mock_client_secret.call_args == call()
+
+        assert mock_reward_cost.call_count == 1
+        assert mock_reward_cost.call_args == call(PLAYER_REGISTRATION_REWARD_PARAM_NAME)
+
     def test_isAllRequiredConfigDefined_nok_clientId(self, mocker):
-        mocker.patch(
+        mock_client_id = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientId',
             return_value=""
         )
-        mocker.patch(
+        mock_client_secret = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientSecret',
             return_value="client_secret"
         )
-        mocker.patch(
+        mock_reward_cost = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getRewardCost',
             return_value=5000
         )
 
         assert configController.isAllRequiredConfigDefined() is False
+
+        assert mock_client_id.call_count == 1
+        assert mock_client_id.call_args == call()
+
+        assert mock_client_secret.call_count == 0
+
+        assert mock_reward_cost.call_count == 0
 
     def test_isAllRequiredConfigDefined_nok_clientSecret(self, mocker):
-        mocker.patch(
+        mock_client_id = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientId',
             return_value="client_id"
         )
-        mocker.patch(
+        mock_client_secret = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientSecret',
             return_value=""
         )
-        mocker.patch(
+        mock_reward_cost = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getRewardCost',
             return_value=5000
         )
 
         assert configController.isAllRequiredConfigDefined() is False
 
+        assert mock_client_id.call_count == 1
+        assert mock_client_id.call_args == call()
+
+        assert mock_client_secret.call_count == 1
+        assert mock_client_secret.call_args == call()
+
+        assert mock_reward_cost.call_count == 0
+
     def test_isAllRequiredConfigDefined_nok_rewardCost(self, mocker):
-        mocker.patch(
+        mock_client_id = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientId',
             return_value="client_id"
         )
-        mocker.patch(
+        mock_client_secret = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getClientSecret',
             return_value="client_secret"
         )
-        mocker.patch(
+        mock_reward_cost = mocker.patch(
             'apps.controllers.ConfigController.user_config_store.getRewardCost',
             return_value=0
         )
 
         assert configController.isAllRequiredConfigDefined() is False
+
+        assert mock_client_id.call_count == 1
+        assert mock_client_id.call_args == call()
+
+        assert mock_client_secret.call_count == 1
+        assert mock_client_secret.call_args == call()
+
+        assert mock_reward_cost.call_count == 1
+        assert mock_reward_cost.call_args == call(PLAYER_REGISTRATION_REWARD_PARAM_NAME)
