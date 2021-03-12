eb6b28148c1371bbc77658b277f4bf5ac0bcf85d
Adaendra <maxime.guilbert@neuf.fr>
2021-03-12 17:46:59 -0500

    WIP - Reward Service

--- apps/services/RewardService.py
+++ apps/services/RewardService.py
@@ -1,5 +1,6 @@
 from apps.services.TwitchRedemptionService import createReward, deleteReward
-from apps.constants.RewardsConstants import PLAYER_REGISTRATION_REWARD_PARAM_NAME, PLAYER_REGISTRATION_REWARD_TITLE
+from apps.constants.RewardsConstants import PLAYER_REGISTRATION_REWARD_PARAM_NAME, PLAYER_REGISTRATION_REWARD_TITLE, \
+    RESPONSE_A_REWARD_TITLE, RESPONSE_B_REWARD_TITLE, RESPONSE_C_REWARD_TITLE, RESPONSE_D_REWARD_TITLE
 from apps.services.stores.UserConfigStore import user_config_store
 from apps.services.stores.RewardIdStore import reward_id_store
 
@@ -11,6 +12,7 @@ Contains all methods related to Twitch Rewards.
 """
 
 
+# ----- Quiz Registration ----- #
 def createQuizRegistrationReward():
     """
     Create the registration reward to allow viewers to be a quiz contestant.
@@ -33,3 +35,59 @@ def deleteQuizRegistrationReward():
 
     reward_id_store.setRegistrationRewardId(None)
 
+
+# ----- Quiz Answers ----- #
+def createQuizAnswersReward():
+    # -- A
+    response_a_reward_id = createReward(
+        RESPONSE_A_REWARD_TITLE,
+        0
+    )
+
+    reward_id_store.setResponseARewardId(response_a_reward_id)
+
+    # -- B
+    response_b_reward_id = createReward(
+        RESPONSE_B_REWARD_TITLE,
+        0
+    )
+
+    reward_id_store.setResponseBRewardId(response_b_reward_id)
+
+    # -- C
+    response_c_reward_id = createReward(
+        RESPONSE_C_REWARD_TITLE,
+        0
+    )
+
+    reward_id_store.setResponseCRewardId(response_c_reward_id)
+
+    # -- D
+    response_d_reward_id = createReward(
+        RESPONSE_D_REWARD_TITLE,
+        0
+    )
+
+    reward_id_store.setResponseDRewardId(response_d_reward_id)
+
+
+def deleteQuizAnswersReward():
+    # -- A
+    deleteReward(reward_id_store.getResponseARewardId())
+
+    reward_id_store.setResponseARewardId(None)
+
+    # -- B
+    deleteReward(reward_id_store.getResponseBRewardId())
+
+    reward_id_store.setResponseBRewardId(None)
+
+    # -- C
+    deleteReward(reward_id_store.getResponseCRewardId())
+
+    reward_id_store.setResponseCRewardId(None)
+
+    # -- D
+    deleteReward(reward_id_store.getResponseDRewardId())
+
+    reward_id_store.setResponseDRewardId(None)
