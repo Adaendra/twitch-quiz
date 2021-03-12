a43b6179a2ffce8c1394c4e9dfe0d6a8345ada75
Adaendra <maxime.guilbert@neuf.fr>
2021-03-12 17:53:17 -0500

    WIP - Reward Id Store

--- tests/services/stores/test_reward_id_store.py
+++ tests/services/stores/test_reward_id_store.py
@@ -14,3 +14,51 @@ class TestRewardIdStore:
         reward_id_store.setRegistrationRewardId("coucou_hibou")
 
         assert reward_id_store.getRegistrationRewardId() == "coucou_hibou"
+
+    # ----- getResponseARewardId ----- #
+    def test_ResponseARewardId(self):
+        reward_id_store = RewardIdStore()
+
+        # Test default value
+        assert reward_id_store.getResponseARewardId() == ""
+
+        # Test Getter and Setter
+        reward_id_store.setResponseARewardId("coucou_hibou")
+
+        assert reward_id_store.getResponseARewardId() == "coucou_hibou"
+
+    # ----- getResponseBRewardId ----- #
+    def test_ResponseBRewardId(self):
+        reward_id_store = RewardIdStore()
+
+        # Test default value
+        assert reward_id_store.getResponseBRewardId() == ""
+
+        # Test Getter and Setter
+        reward_id_store.setResponseBRewardId("coucou_hibou")
+
+        assert reward_id_store.getResponseBRewardId() == "coucou_hibou"
+
+    # ----- getResponseARewardId ----- #
+    def test_ResponseCRewardId(self):
+        reward_id_store = RewardIdStore()
+
+        # Test default value
+        assert reward_id_store.getResponseCRewardId() == ""
+
+        # Test Getter and Setter
+        reward_id_store.setResponseCRewardId("coucou_hibou")
+
+        assert reward_id_store.getResponseCRewardId() == "coucou_hibou"
+
+    # ----- getResponseDRewardId ----- #
+    def test_ResponseDRewardId(self):
+        reward_id_store = RewardIdStore()
+
+        # Test default value
+        assert reward_id_store.getResponseDRewardId() == ""
+
+        # Test Getter and Setter
+        reward_id_store.setResponseDRewardId("coucou_hibou")
+
+        assert reward_id_store.getResponseDRewardId() == "coucou_hibou"
