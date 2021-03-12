76d36c3f023a2d6fdc1af19911b0b6df7475c035
Adaendra <maxime.guilbert@neuf.fr>
2021-03-12 17:59:55 -0500

    WIP - Ajout route startQuiz

--- apps/routes/socket/QuizRoutes.py
+++ apps/routes/socket/QuizRoutes.py
@@ -1,6 +1,6 @@
 from apps import socketio
 from apps.controllers.QuizController import quizController
-from apps.constants.SocketMessageTypeConstants import SOCKET_REQUEST_QUIZ_INIT
+from apps.constants.SocketMessageTypeConstants import SOCKET_REQUEST_QUIZ_INIT, SOCKET_REQUEST_START_QUIZ
 
 
 class QuizRoutes(object):
@@ -17,3 +17,10 @@ class QuizRoutes(object):
         Initialize quiz.
         """
         quizController.init_quiz()
+
+    @socketio.on(SOCKET_REQUEST_START_QUIZ)
+    def startQuiz():
+        """
+        Start quiz.
+        """
+        quizController.start_quiz()
