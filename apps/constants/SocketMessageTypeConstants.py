f202433b400a1e27ba6ca383daeac0222aae964c
Adaendra <maxime.guilbert@neuf.fr>
2021-03-14 20:25:33 -0400

    WIP - Process answers

--- apps/constants/SocketMessageTypeConstants.py
+++ apps/constants/SocketMessageTypeConstants.py
@@ -10,5 +10,6 @@ SOCKET_EVENT_STATS_CONSTESTANTS_CHECK_IN = "event:stats:contestants_check_in"
 SOCKET_EVENT_STATS_ANSWERS_ONGOING = "event:stats:answers_ongoing"
 SOCKET_EVENT_STATS_ANSWERS_QUESTION = "event:stats:answers_question"
 # Quiz
+SOCKET_EVENT_REVEAL_ANSWER = "event:quiz:reveal_answer"
 SOCKET_EVENT_QUIZ_NEXT_QUESTION = "event:quiz:next_question"
 SOCKET_EVENT_QUIZ_STOP = "event:quiz:stop"
